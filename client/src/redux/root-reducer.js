4d1f62d1c9314fb671bcb326679a32c5bedab66c
Andrew Campbell <andrew@polychlorinated.com>
2024-07-26 22:09:47 -0500

    Fix Socket Reducer import issue

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -2,13 +2,17 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user-reducer";
 import projectReducer from "./project/project-reducer";
 import epicReducer from "./epic/epic-reducer";
-import socketReducer from "./socket/socket-reducer";
+//import socketReducer from "./socket/socket-reducer";
+import authReducer from './auth/auth-reducer';
+import projectReducer from './project/project-reducer';
 
 const rootReducer = combineReducers({
+  auth: authReducer,
+  project: projectReducer,
   userState: userReducer,
   projectState: projectReducer,
   epicState: epicReducer,
-  socketState: socketReducer,
+  //socketState: socketReducer,
 });
 
 export default rootReducer;
